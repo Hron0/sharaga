aaa1421e666dfb0133d5ec1320cd768b42a755f6
Hron0 <sasha1254370@gmail.com>
2024-01-25 19:28:15 +0300

    yes

---
+++ initial_prj/resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+@section('title', 'Page Title')
+
+@section('body')
+    <h1 class="text-black">Welcome to my website!</h1>
+    <p class="text-green-500">This is the content of my website.</p>
+@endsection
