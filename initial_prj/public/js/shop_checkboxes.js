a4a603b95c419ab56e74a8d1cd65467763796578
Hron0 <sasha1254370@gmail.com>
2024-01-28 23:55:31 +0300

    bit of refactoring

--- initial_prj/public/js/shop_checkboxes.js
+++ initial_prj/public/js/shop_checkboxes.js
@@ -1,49 +1,30 @@
 // @ts-nocheck
 window.onload = function () {
   console.log("I HATE NIGGERS")
-  // Get references to the checkboxes
-  var inStockCheckbox = document.getElementById('in_stock');
-  var outStockCheckbox = document.getElementById('out_stock');
+
+  //Checkboxes part
   var resetBtn = document.getElementById('no_fltr');
   var form = document.getElementById('products_form');
 
-  // Function to handle the checkbox click event
-  function toggleFirst() {
-    if (inStockCheckbox.checked && outStockCheckbox.checked) {
-      outStockCheckbox.checked = false
-    }
-  }
+  document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+      document.querySelectorAll('input[type="checkbox"]').forEach(otherCheckbox => {
+        if (otherCheckbox !== checkbox) {
+          otherCheckbox.checked = false;
+        }
+      });
+    });
+  });
+  //END: Checkboxes part
 
-  function toggleSecond() {
-    if (inStockCheckbox.checked && outStockCheckbox.checked) {
-      inStockCheckbox.checked = false
-    }
-  }
 
+  //Reset Availability-filter
   function resetFilters() {
     outStockCheckbox.checked = false
     inStockCheckbox.checked = false
 
-    console.log("BLACKKCCKASDA")
-
     form.submit()
   }
 
-  // Attach the event listener to both checkboxes
-  inStockCheckbox.addEventListener('change', toggleFirst);
-  outStockCheckbox.addEventListener('change', toggleSecond);
   resetBtn.addEventListener('click', resetFilters);
-
-  console.log("Slider connected")
-  const slider = document.getElementById('default-range')
-
-  const fncSlider = () => {
-    console.log(slider.value)
-  }
-
-  slider.addEventListener('change', fncSlider)
 }
-
-
-/* // Make sure the DOM is fully loaded before running the script
-document.addEventListener('DOMContentLoaded', addToggleFunctionality); */
