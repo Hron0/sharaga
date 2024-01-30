245cf9aaddb3ef657ae24566a8e0e64f82013f56
Hron0 <sasha1254370@gmail.com>
2024-01-30 22:45:05 +0300

    Some kind of Auth sys

--- initial_prj/app/Http/Controllers/LoginController.php
+++ initial_prj/app/Http/Controllers/LoginController.php
@@ -3,10 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function showPage() {
+    public function showPage()
+    {
         return view('auth.login');
     }
+
+    public function login(Request $req)
+    {
+        $credentials = $req->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $req->session()->regenerate();
+            return redirect()->route('shop')
+                ->withSuccess('You have successfully logged in!');
+        }
+
+        return back()->withErrors([
+            'error' => 'Either password or email is wrong. Bozo',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $req)
+    {
+        Auth::logout();
+        $req->session()->invalidate();
+        $req->session()->regenerateToken();
+        return redirect()->route('login')
+            ->withSuccess('Logged out');;
+    }  
 }
--- initial_prj/app/Http/Controllers/RegisterController.php
+++ initial_prj/app/Http/Controllers/RegisterController.php
@@ -2,15 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class RegisterController extends Controller
 {
-    public function showPage() {
+
+/*     public function __construct()
+    {
+        $this->middleware('guest')->except([
+            'logout', 'dashboard'
+        ]);
+    } */
+
+
+    public function showPage()
+    {
         return view('auth.register');
     }
 
-    public function register() {
-        
+    public function register(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'name' => 'required|string|max:250',
+            'email' => 'required|email|max:250|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/register')
+                ->withErrors($validator);
+        }
+
+        User::create([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password)
+        ]);
+
+        $credentials = $req->only('email', 'password');
+        Auth::attempt($credentials);
+        $req->session()->regenerate();
+        return redirect()->route('shop')
+            ->withSuccess('You have successfully registered & logged in!');
     }
 }
