1b6bc1b493f4c4bd530fe83a4c39d7f50da93793
Hron2 <saaha1254370@gmail.com>
2024-02-21 14:38:28 +0300

    UPD Migration

--- initial_prj/database/migrations/2024_02_21_113223_create_cart_items_table.php
+++ initial_prj/database/migrations/2024_02_21_113223_create_cart_items_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('guitars')->onDelete('cascade');
         });
     }
 
